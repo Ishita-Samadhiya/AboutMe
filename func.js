4aad197a19774ee7220a6ceda315b9cf922ab11c
StudentAM <studentam@space.metroed.local>
2023-09-14 10:30:13 -0700

    int

--- func.js
+++ func.js
@@ -6,3 +6,8 @@ function scrollRotate() {
     let image = document.getElementById("home");
     image.style.transform = "rotate(" + window.scrollY/2 + "deg)";
 }
+// JavaScript function to toggle the navigation menu
+function openNavbar() {
+	const navMenu = document.getElementById("nav-menu");
+	navMenu.classList.toggle("active");
+}
